08ad560a66d9bb5fb645d5d8dee7f7c57e820699
Jacki <230686940+giacomobarriera@users.noreply.github.com>
2026-01-02 18:00:16 +0100

    Creata la griglia.

--- javascript.js
+++ javascript.js
@@ -1,4 +1,23 @@
 //prendere container
 const container = document.querySelector(".container");
+console.log(container)
 
-//
+//creare grid 16 x 16
+const rowBox = function () {
+    //ciclo per row
+    for (let i = 1; i < 17; i++) {
+    const row = document.createElement("div");
+    container.appendChild(row);
+    row.classList.add("row");
+    //ciclo per box
+    for (let i = 1; i < 17; i++) {
+        const a = document.createElement("div");
+        row.appendChild(a);
+        a.textContent = "a" + i;
+        a.classList.add("gridBox");
+    }
+   }
+}
+
+ 
+rowBox();
