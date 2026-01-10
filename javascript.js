5c074f3fe73b213d4d067f2290ad9611fd65bcf6
Jacki <230686940+giacomobarriera@users.noreply.github.com>
2026-01-10 18:50:16 +0100

    Aggiunto tentativo di modifica grid attraverso prompt.
    Quando scrivo il numero nella barra, vengono prodotte
    rows nel grid, senza cambiare misura.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+
 //prendere form
 const form = document.getElementById("myForm");
 console.log(form)
@@ -11,11 +12,42 @@ console.log(container)
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     const numero = document.getElementById("Numero").value;
-    console.log(numero);
+    //trasformo in numero la costante
+    const numeroInt = parseInt(numero);
+    console.log(typeof(numero));
+    console.log(typeof(numeroInt));
+    console.log(numeroInt);
+
+    //se diverso da numero, alert
+    if (isNaN(numeroInt)) {
+        alert("Attento! Assicurati di inserire un numero!")
+    } else {
+        for (let i = 1; i <= numeroInt; i++) {
+            const row = document.createElement("div");
+            container.appendChild(row);
+            row.classList.add("row");
+            //verifica
+            console.log(row);
+    
+            //ciclo per box
+        /*for (let i = 1; i < 17; i++) {
+            const a = document.createElement("div");
+            row.appendChild(a);
+            a.classList.add("gridBox");
+            a.addEventListener("mouseenter", () => {
+                a.classList.add("active");
+            })
+            a.addEventListener("mouseleave", () => {
+                a.classList.remove("active");
+                a.classList.add("pixel");
+            })
+        }*/
+    }
+}
+
 })
-
 //creare grid 16 x 16
-
+/*
     //ciclo per row
     for (let i = 1; i < 17; i++) {
     const row = document.createElement("div");
@@ -36,5 +68,4 @@ form.addEventListener("submit", (e) => {
         
     }
    }
-
-   
+*/
